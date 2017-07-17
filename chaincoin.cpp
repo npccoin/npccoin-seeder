c10694943becc3df2b1f51b7f96794557a632d7f
adminatriviss <atrivissapps@gmail.com>
2017-07-17 16:38:33 +0100

    Initial commit

--- chaincoin.cpp
+++ chaincoin.cpp
@@ -298,7 +298,7 @@ bool TestNode(const CService &cip, int &ban, int &clientV, std::string &clientSV
 
 /*
 int main(void) {
-  CService ip("chaincoin.sipa.be", 8333, true);
+  CService ip("chaincoin.org", 8333, true);
   vector<CAddress> vAddr;
   vAddr.clear();
   int ban = 0;
